7a2a997cee4e97e7820814f53de398b5d0267dc1
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-09-19 12:48:57 +0500

    Task4

---
+++ Tyuiu.ErmakovAA.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tyuiu.ErmakovAA.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        // пример линейной структуры 
+
+        public static int Addition(int a, int b)
+            { return a + b; }
+
+        public static int Subtraction(int a, int b)
+        { return a - b; }
+
+        public static int Multiplication(int a, int b)
+        { return a * b; }
+
+        public static int Division(int a, int b)
+        { return a / b; }
+
+    }
+}
---
+++ Tyuiu.ErmakovAA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.ErmakovAA.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.ErmakovAA.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // пример линейной структуры программы 
+            //вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+
+            Console.WriteLine(DataService.Subtraction(7, 5));
+
+            Console.WriteLine(DataService.Multiplication(7, 7));
+
+            Console.WriteLine(DataService.Division(50, 5));
+
+            Console.ReadKey();
+        }
+    }
+}

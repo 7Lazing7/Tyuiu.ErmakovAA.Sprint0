d6198068c4ba4739daf8e5f2ebbfecbc275fee31
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-09-19 11:51:31 +0500

    Task3

---
+++ Tyuiu.ErmakovAA.Sorint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,9 @@
+﻿namespace Tyuiu.ErmakovAA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        //реализация метода Sum
+        public static int Sum (int a, int b)
+            { return a + b;}
+    }
+}

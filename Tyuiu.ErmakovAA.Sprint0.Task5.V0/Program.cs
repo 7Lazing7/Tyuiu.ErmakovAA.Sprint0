58a949bf8d195dda6a6f8aa7a176e92af4fd594f
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-09-30 21:56:36 +0500

    Task5

---
+++ Tyuiu.ErmakovAA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.ErmakovAA.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.ErmakovAA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // пример линейной структуры программы 
+            //вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+
+            Console.WriteLine(DataService.Subtraction(7, 5));
+
+            Console.WriteLine(DataService.Multiplication(7, 7));
+
+            Console.WriteLine(DataService.Division(0, 6));
+
+            Console.ReadKey();
+        }
+    }
+}
